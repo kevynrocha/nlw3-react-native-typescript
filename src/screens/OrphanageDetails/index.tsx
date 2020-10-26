4e5db435b46b706b6733312a7f1b636a165722f3
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-26 18:54:52 -0400

    feat: add OrphanageDetails component

--- src/screens/OrphanageDetails/index.tsx
+++ src/screens/OrphanageDetails/index.tsx
@@ -1,14 +1,130 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, ScrollView, View, Linking } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import mapMarkerImg from '../../assets/images/map-marker.png';
 
-const OrphanageDetails: React.FC = () => {
+import * as S from './styles';
+import api from '../../services/api';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+interface OrphanageDetailsRouteParams {   
+  id: number;  
+}
+
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+  images: Array<{
+    id: number;
+    url: string;
+  }>
+}
+
+export default function OrphanageDetails() {
+  const [orphanage, setOrphanage] = useState<Orphanage>();
+  const route = useRoute();
+
+  const params = route.params as OrphanageDetailsRouteParams;
+
+  useEffect(() => {
+    const fetchOrphanage = async () => {
+      const response = await api.get(`orphanages/${params.id}`);
+      setOrphanage(response.data);
+    }
+    fetchOrphanage();
+  }, [params.id])
+
+  const handleOpenGoogleMapsRoutes = () => {
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&destination=${orphanage?.latitude}, ${orphanage?.longitude}`)
+  }
+
+  if (!orphanage) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <ActivityIndicator size="large" color="#FFD666" /> 
+      </View>
+    )
+  }
+
   return (
-    <Container>
-      <Text>OrphanageDetails</Text>
-    </Container>
-  );
-};
+    <S.Container>
+      <S.ImagesContainer>
+        <ScrollView horizontal pagingEnabled>
+          {orphanage.images.map(image => (
+            <S.Image key={image.id} source={{ uri: image.url }} />
+          ))}
+        </ScrollView>
+      </S.ImagesContainer>
 
-export default OrphanageDetails;
+      <S.DetailsContainer>
+        <S.Title>{orphanage.name}</S.Title>
+        <S.Description>{orphanage.about}</S.Description>
+      
+        <S.MapContainer>
+          <MapView 
+            initialRegion={{
+              latitude: orphanage.latitude,
+              longitude: orphanage.longitude,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
+            }} 
+            zoomEnabled={false}
+            pitchEnabled={false}
+            scrollEnabled={false}
+            rotateEnabled={false}
+            style={{
+              width: '100%',
+              height: 150
+            }}
+          >
+            <Marker 
+              icon={mapMarkerImg}
+              coordinate={{ 
+                latitude: orphanage.latitude,
+                longitude: orphanage.longitude
+              }}
+            />
+          </MapView>
+
+          <S.RoutesContainer>
+            <TouchableOpacity onPress={handleOpenGoogleMapsRoutes}>
+              <S.RoutesText>Ver rotas no Google Maps</S.RoutesText>
+            </TouchableOpacity>
+          </S.RoutesContainer>
+        </S.MapContainer>
+      
+        <S.Separator />
+
+        <S.Title>Instruções para visita</S.Title>
+        <S.Description>{orphanage.instructions}</S.Description>
+
+        <S.ScheduleContainer>
+          <S.ScheduleItemBlue>
+            <Feather name="clock" size={40} color="#2AB5D1" />
+            <S.ScheduleTextBlue>Segunda à Sexta {orphanage.opening_hours}</S.ScheduleTextBlue>
+          </S.ScheduleItemBlue>
+          {orphanage.open_on_weekends ? (
+            <S.ScheduleItemGreen>
+              <Feather name="info" size={40} color="#39CC83" />
+              <S.ScheduleTextGreen>Atendemos fim de semana</S.ScheduleTextGreen>
+            </S.ScheduleItemGreen> ) 
+          : (
+            <S.ScheduleItemRed>
+              <Feather name="info" size={40} color="#FF669D" />
+              <S.ScheduleTextRed>Não atendemos fim de semana</S.ScheduleTextRed>
+            </S.ScheduleItemRed>
+          )}
+        </S.ScheduleContainer>        
+      </S.DetailsContainer>
+    </S.Container>
+  )
+}
--- src/screens/OrphanageDetails/styles.ts
+++ src/screens/OrphanageDetails/styles.ts
@@ -1,5 +1,108 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
 
+export const ImagesContainer = styled.View`
+  height: 240px;
+`;
+
+export const Image = styled.Image`
+  width: ${Dimensions.get('window').width};
+  height: 240px;
+  resize-mode: cover;
+`;
+
+export const DetailsContainer = styled.View`
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  color: #4D6F80;
+  font-size: 30px;
+  font-family: 'Nunito_700Bold';
+`;
+
+export const Description = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  color: #5c8599;
+  line-height: 24px;
+  margin-top: 16px;
+`;
+
+export const MapContainer = styled.View`
+  border-radius: 20px;
+  overflow: hidden;
+  border-width: 1.2px;
+  border-color: #B3DAE2;
+  margin-top: 40px;
+  background-color: #E6F7FB;
+`;
+
+export const RoutesContainer = styled.View`
+  padding: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RoutesText = styled.Text`
+  font-family: 'Nunito_700Bold';
+  color: #0089a5;
+`;
+
+export const Separator = styled.View`  
+  height: 0.8px;
+  width: 100%;
+  background-color: #D3E2E6;
+  margin: 40px 0;  
+`;
+
+export const ScheduleContainer = styled.View`  
+  margin-top: 24px;
+  flex-direction: row;
+  justify-content: space-between; 
+`;
+
+export const ScheduleItem = styled.View`  
+  width: 48%;
+  padding: 20px;
+  border-width: 1px;
+  border-radius: 20px;
+`;
+
+export const ScheduleItemBlue = styled(ScheduleItem)`
+  background-color: #E6F7FB;  
+  border-color: #B3DAE2;  
+`;
+
+export const ScheduleItemGreen = styled(ScheduleItem)`
+  background-color: #EDFFF6;
+  border-color: #A1E9C5;
+`;
+
+export const ScheduleItemRed = styled(ScheduleItem)`
+  background-color: #FFE4EE;
+  border-color: #FF669D;
+`;
+
+export const ScheduleText = styled.Text`  
+  font-family: 'Nunito_600SemiBold';
+  font-size: 16px;
+  line-height: 24px;
+  margin-top: 20px;
+`;
+
+export const ScheduleTextBlue = styled(ScheduleText)`  
+  color: #5C8599;
+`;
+
+export const ScheduleTextGreen = styled(ScheduleText)`  
+  color: #37C77F;
+`;
+
+export const ScheduleTextRed = styled(ScheduleText)`  
+  color: #FF669D;
 `;
