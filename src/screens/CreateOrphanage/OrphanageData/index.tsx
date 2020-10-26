99723dc8172e1ce651cc65143bc3645c3db80bc1
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-26 18:54:31 -0400

    feat: add OrphanageData component

---
+++ src/screens/CreateOrphanage/OrphanageData/index.tsx
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { Switch } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+import { Container, Title, NextButtonText, NextButton, Comment, ImagesInput, Input, Label, SwitchContainer, UploadedImagesContainer, UploadedImage } from './styles';
+import api from '../../../services/api';
+
+interface OrphanageDataRouteParams {
+  position: { latitude: number, longitude: number};
+}
+
+export default function OrphanageData() {
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekends, setOpenOnWeekends] = useState(false);
+  const [images, setImages] = useState<string[]>([]);
+
+  const route = useRoute();
+  const navigation = useNavigation(); 
+
+  const params = route.params as OrphanageDataRouteParams;
+  const position = params.position;
+
+  const handleCreateOrphanage = async () => {
+    const { latitude, longitude } = params.position;
+
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+    images.forEach((image, index) => {
+      data.append('images', {
+        name: `image_${index},jpg`,
+        type: 'image/jpg',
+        uri: image
+      } as any)
+    })
+
+    await api.post('orphanages', data);
+
+    navigation.navigate('OrphanagesMap');
+  }
+
+  async function handleSelectImages() {
+    const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (status !== 'granted') {
+      alert('Eita! Precisamos de acesso às suas fotos...');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images
+    });
+
+    if (result.cancelled) {
+      return;
+    }
+
+    const { uri: image } = result;
+
+    setImages([...images, image]);
+  }
+
+  return (
+    <Container contentContainerStyle={{ padding: 24 }}>
+      <Title>Dados</Title>
+
+      <Label>Nome</Label>
+      <Input 
+        value={name}
+        onChangeText={setName}
+      />
+
+      <Label>Sobre</Label>
+      <Input
+        style={{ height: 110 }}
+        multiline
+        value={about}
+        onChangeText={setAbout}
+      />
+      
+      <Label>Fotos</Label>
+
+      <UploadedImagesContainer>
+        {images.map(image => (
+          <UploadedImage 
+            key={image}
+            source={{ uri: image }}
+          />
+        ))}
+      </UploadedImagesContainer>
+
+      <ImagesInput onPress={handleSelectImages}>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </ImagesInput>
+      
+      <Title>Visitação</Title>
+
+      <Label>Instruções</Label>
+      <Input
+        style={{ height: 110 }}
+        multiline
+        value={instructions}
+        onChangeText={setInstructions}
+      />
+
+      <Label>Horário de visitas</Label>
+      <Input
+        value={opening_hours}
+        onChangeText={setOpeningHours}
+      />
+
+      <SwitchContainer>
+        <Label>Atende final de semana?</Label>
+        <Switch 
+          thumbColor="#fff" 
+          trackColor={{ false: '#ccc', true: '#39CC83' }}
+          value={open_on_weekends}
+          onValueChange={setOpenOnWeekends}
+        />
+      </SwitchContainer>
+
+      <NextButton onPress={handleCreateOrphanage}>
+        <NextButtonText> Cadastrar</NextButtonText>
+      </NextButton>
+    </Container>
+  )
+}
